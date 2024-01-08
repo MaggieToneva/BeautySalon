d5205c9579c79e5f383169a51d1f2994ba1782fa
smgBA8F <magetinka@gmail.com>
2024-01-08 19:14:15 +0200

    Init

---
+++ src/beautysalon/BeautySalon.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beautysalon;
+
+/**
+ *
+ * @author smgBA8F
+ */
+public class BeautySalon {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
